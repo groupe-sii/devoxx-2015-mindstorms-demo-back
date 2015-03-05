0add7b93b22dd88786f83f3b89177c875cb97848
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-03-05 14:46:27 +0100

    [TECH] ev3Dev module added, start motor route configured

--- motor.js
+++ motor.js
@@ -1,8 +1,25 @@
 'use strict';
 
+var ev3dev = require('ev3dev'),
+    motor = new ev3dev.Motor(),
+    motorOnDuration = 1000;
+
 module.exports = {
     start: function(req, res, next) {
         console.log('start motor');
-        res.send(true);
+        // Up ramp
+        motor.rampUpSp = 200;
+        // Down ramp
+        motor.rampDownSp = 200;
+        // Run mode
+        motor.runMode = 'time';
+        // Time spend
+        motor.timeSp = motorOnDuration;
+        // Power
+        motor.dutyCycleSp = 75;
+        motor.run = 1;
+        setTimeout(function() {
+            res.send(true);
+        }, motorOnDuration);
     }
 };

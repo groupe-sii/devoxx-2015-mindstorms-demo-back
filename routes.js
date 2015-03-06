eb5b216dface67dad4aae1a75d6ed32359d4055c
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-03-06 09:31:22 +0100

    [TECH] Cleaning

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@
 
 var motorEndpoint   = require('./motor.js'),
     sensorEndpoint  = require('./sensor.js'),
-    cors            = require('cors'),
     API_VERSION     = '/v1',
     API_ENTRYPOINT  = '/api';
 

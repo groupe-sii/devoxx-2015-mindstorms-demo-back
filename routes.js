5e5d3480d490bf5ba77ee6d684ef9df1883fee4c
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-03-06 10:55:54 +0100

    [TECH] Readd cors

--- routes.js
+++ routes.js
@@ -2,10 +2,13 @@
 
 var motorEndpoint   = require('./motor.js'),
     sensorEndpoint  = require('./sensor.js'),
+    cors            = require('cors'),
     API_VERSION     = '/v1',
     API_ENTRYPOINT  = '/api';
 
 module.exports = function(app, io) {
+    app.use(cors());
+
     app.route(API_ENTRYPOINT + API_VERSION + '/motor')
         .put(motorEndpoint.start);
 

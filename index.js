d7ac5e2130cc9068bbd5eed0a4d84d0018cb3488
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-04-07 13:42:56 +0200

    [TECH] Detect return key for exiting server

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 /**
  * Setup
  */
-var express = require('express'),
-    app     = express(),
-    http    = require('http').Server(app),
-    io      = require('socket.io')(http),
-    port    = 3000;
+var express     = require('express'),
+    app         = express(),
+    http        = require('http').Server(app),
+    io          = require('socket.io')(http),
+    keypress    = require('keypress'),
+    port        = 3000;
 
 /**
  * Configuration
@@ -23,3 +24,16 @@ require('./routes.js')(app, io);
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + port);
 });
+
+keypress(process.stdin);
+
+process.stdin.on('keypress', function(ch, key) {
+    console.log('got "keypress"', key);
+    if (key && key.name === 'return') {
+        process.stdin.pause();
+        process.exit();
+    }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
